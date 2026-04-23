edf200fad3138cc80ff29fdbb35b082a382df04b
Harpreet-Singh413 <harpreetsinghbassan0307@gmail.com>
2026-04-23 16:12:57 +0530

    feat : enable Scheduling

--- Spring-Boot-Backend/src/main/java/com/example/leetboardPro/LeetboardProApplication.java
+++ Spring-Boot-Backend/src/main/java/com/example/leetboardPro/LeetboardProApplication.java
@@ -3,9 +3,11 @@ package com.example.leetboardPro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableScheduling
 public class LeetboardProApplication {
 
 	public static void main(String[] args) {
